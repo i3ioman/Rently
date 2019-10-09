1c403808e359c65100bb5b4742bb2b7b1d03374c
Rafał Dycha <chiroxrs@gmail.com>
2019-10-09 14:28:12 +0200

    Initial Commit

---
+++ Rently/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rently
+//
+//  Created by Rafał Dycha on 09/10/2019.
+//  Copyright © 2019 Rafał Dycha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
